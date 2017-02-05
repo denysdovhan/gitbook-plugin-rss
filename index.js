1b6843a9532242f8eb8a95a6ae1e0f89b5327914
Denys Dovhan <email@denysdovhan.com>
2017-02-05 15:14:25 +0200

    Get rid of Babel

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-import title from 'get-md-title';
-import desc  from 'get-md-desc';
-import RSS   from 'rss';
-import { writeFileSync as write } from 'fs';
-import { resolve, basename }      from 'path';
-import { parse }                  from 'url';
+const title = require('get-md-title');
+const desc  = require('get-md-desc');
+const RSS   = require('rss');
+const { writeFileSync: write } = require('fs');
+const { resolve, basename } = require('path');
+const { parse } = require('url');
 
 // Define variables
 let site, feed;
 
-export default {
+module.exports = {
   website: {
     assets: './assets',
-    js: [ 'plugin.js' ]
+    js: [ 'plugin.js' ],
   },
 
   hooks: {
     // Get and init RSS configuration
-    'init': function () {
+    init() {
       site = this.config.get('pluginsConfig.rss');
       feed = new RSS(site);
     },
 
     // Collect all pages
-    'page:before': function (page) {
+    ['page:before'](page) {
       // If README.md, then change it to root
       const url = site.site_url +
         ( page.path === 'README.md'
         ? ''
-        : page.path.replace(/.md$/,'.html'));
+        : page.path.replace(/.md$/, '.html'));
 
       const pageTitle = title(page.content);
       const pageDescription = desc(page.content);
 
       feed.item({
-        title: pageTitle? pageTitle.text : '',
-        description: pageDescription? pageDescription.text : '',
+        title: pageTitle ? pageTitle.text : '',
+        description: pageDescription ? pageDescription.text : '',
         url: url,
-        author: site.author
+        author: site.author,
       });
 
       return page;
     },
 
     // Generate XML and write to file
-    'finish': function () {
+    finish() {
       const xml = feed.xml({ indent: true });
       const feedpath = basename(parse(site.feed_url).pathname);
       return write(resolve(this.options.output, feedpath), xml, 'utf-8');
-    }
+    },
   }
 };
